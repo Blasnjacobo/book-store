bc238b415f2e79dc11aa5704cfb369b6aa1c9cf9
Blasnjacobo <blasnjacobo05@gmail.com>
2024-02-14 11:08:50 -0700

    book-store

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import BlackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
@@ -49,6 +48,27 @@ const CreateBooks = () => {
             className='border-2 border-gray-500 px-4 py-2 w-full'
           />
         </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input
+            type='text'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input
+            type='text'
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+          Save
+        </button>
       </div>
     </div>
   )
